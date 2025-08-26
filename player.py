f25db890f59ee6655e11454121d39f0295b84f71
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-26 11:06:42 -0500

    add movement and position reset methods to Player class

--- player.py
+++ player.py
@@ -14,4 +14,13 @@ class Player(Turtle):
         self.setheading(90)
 
 
+    def move(self):
+        self.forward(MOVE_DISTANCE)
 
+    def reset_position(self):
+        self.goto(STARTING_POSITION)
+    def finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
