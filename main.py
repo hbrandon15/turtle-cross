68059f3a7287b41bd88c8def87614c1f3d3ce975
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2025-08-26 11:12:17 -0500

    create player instance and add keyboard bindings for movement

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+# create the player
+player = Player()
+
+# Keyboard bindings
+screen.listen()
+screen.onkey(player.move, "Up")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+
+
+
 
 
 # first we need to create a turtle player that starts at the bottom of the screen and listens for the "UP" keyword.
